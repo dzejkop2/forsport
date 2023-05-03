e3a19751500bf385acf13612a221ac24f64669b9
SleepFoxx <jakubhulin30@gmail.com>
2023-05-03 21:34:05 +0200

    Prvý commit, nuget
    Databazka no, nejako hadam pojde, ešte to domyslíme, ale hlavne, už sme začali, a to sa cení najviac

---
+++ ForSport/ForSport/Program.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForSport
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+            string host = "geleta.vybrat.eu";
+            string databazka = "c46ForSport";
+            string user = "c46jakub_geleta";
+            string password = "Jakub2544444";
+            string constring = "SERVER=" + host + ";" + "DATABASE=" + databazka + ";" + "UID=" + user + ";" + "PASSWORD=" + password;
+            MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+            string command = "select * from test123";
+            MySqlCommand cmd = new MySqlCommand(command, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            
+
+        }
+    }
+}
