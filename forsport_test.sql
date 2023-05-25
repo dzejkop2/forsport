99c5d0b27bfa8ab5b5c310de97539a76f25713ea
SHim0n10 <49526116+SHim0n10@users.noreply.github.com>
2023-05-25 15:19:48 +0200

    zmena databazy
    kvoli pridaniu kurzov

--- forsport_test.sql
+++ forsport_test.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 23, 2023 at 06:06 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Hostiteľ: 127.0.0.1
+-- Čas generovania: Št 25.Máj 2023, 15:19
+-- Verzia serveru: 10.4.25-MariaDB
+-- Verzia PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `forsport_test`
+-- Databáza: `forsport_test`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forum`
+-- Štruktúra tabuľky pre tabuľku `forum`
 --
 
 CREATE TABLE `forum` (
@@ -34,7 +34,7 @@ CREATE TABLE `forum` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `forum`
+-- Sťahujem dáta pre tabuľku `forum`
 --
 
 INSERT INTO `forum` (`username`, `comment`, `date`) VALUES
@@ -43,7 +43,32 @@ INSERT INTO `forum` (`username`, `comment`, `date`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_balance`
+-- Štruktúra tabuľky pre tabuľku `kurzy`
+--
+
+CREATE TABLE `kurzy` (
+  `first_team` text NOT NULL,
+  `second_team` text NOT NULL,
+  `date` text NOT NULL,
+  `kurz1` text NOT NULL,
+  `kurz10` text NOT NULL,
+  `kurz0` text NOT NULL,
+  `kurz02` text NOT NULL,
+  `kurz2` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Sťahujem dáta pre tabuľku `kurzy`
+--
+
+INSERT INTO `kurzy` (`first_team`, `second_team`, `date`, `kurz1`, `kurz10`, `kurz0`, `kurz02`, `kurz2`) VALUES
+('amerika', 'nemecko', '15:00 15.5. 1944', '3.2', '1.5', '2.3', '4.5', '8.7'),
+('taliansko', 'holandsko', 'vcera vecer', '5.1', '9.8', '11.5', '4.56', '11.2');
+
+-- --------------------------------------------------------
+
+--
+-- Štruktúra tabuľky pre tabuľku `user_balance`
 --
 
 CREATE TABLE `user_balance` (
@@ -52,7 +77,7 @@ CREATE TABLE `user_balance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_balance`
+-- Sťahujem dáta pre tabuľku `user_balance`
 --
 
 INSERT INTO `user_balance` (`id`, `balance`) VALUES
@@ -61,7 +86,7 @@ INSERT INTO `user_balance` (`id`, `balance`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_info`
+-- Štruktúra tabuľky pre tabuľku `user_info`
 --
 
 CREATE TABLE `user_info` (
@@ -72,7 +97,7 @@ CREATE TABLE `user_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_info`
+-- Sťahujem dáta pre tabuľku `user_info`
 --
 
 INSERT INTO `user_info` (`id`, `username`, `password`, `mail`) VALUES
